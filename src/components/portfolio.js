cde723f36bcc18f8f854c09c9400f42789f3b908
LisaTLe <lisa.le1127@gmail.com>
2022-09-24 07:47:58 -0600

    add photos and update portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,13 +1,32 @@
 import React from "react";
-import Navbar from "./navbar";
-import Footer from "./footer";
 
 function portfolio() {
   return (
-    <div className="portfolio" id="portfolio">
-      <Navbar />
-      Hello from portfolio component
-      <Footer />
+    <div className="portfolio-body" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul className="port-list">
+        <li className="port-items">Featured</li>
+        <li className="port-items">Featured</li>
+        <li className="port-items">Featured</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/quiz.PNG" alt="" className="quiz" />
+          <h5>Timed Quiz</h5>
+        </div>
+        <div className="item">
+          <img src="assets/workout.PNG" alt="" className="workout" />
+          <h5>Abs-olutely Workout Guide</h5>
+        </div>
+        <div className="item">
+          <img src="assets/quiz.PNG" alt="" className="quiz" />
+          <h5>Timed Quiz</h5>
+        </div>
+        <div className="item">
+          <img src="assets/quiz.PNG" alt="" className="quiz" />
+          <h5>Timed Quiz</h5>
+        </div>
+      </div>
     </div>
   );
 }
