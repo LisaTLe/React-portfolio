6016757450b556ddd36a7bb5341f8e19104de0e5
LisaTLe <lisa.le1127@gmail.com>
2022-09-23 22:47:07 -0600

    edit homepage and styling

--- src/components/about.js
+++ src/components/about.js
@@ -4,12 +4,7 @@ import "../App.css";
 export default function about() {
   return (
     <div className="about-info" id="about">
-      <div className="about-left">
-        <div className="imgContainer">
-          
-        </div>
-      </div>
-      <div className="about-right"></div>
+      about here
     </div>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,30 @@
 import React from "react";
-import Navbar from "./navbar";
-import Footer from "./footer";
+import "../App.css";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 
 function home() {
   return (
-    <div className="home" id="home">
-      <div>Portfolio</div>
-      <Navbar />
-      Hello, meet Lisa Le!
-      <Footer />
+    <div className="home-info" id="home">
+      <div className="home-left">
+        <div className="imgContainer">
+          <img src="assets/me.PNG" alt="headshot" className="profile-img" />
+        </div>
+      </div>
+      <div className="home-right">
+        <div className="wrapper-home">
+          <h2>Hello, meet</h2>
+          <h1>Lisa Le</h1>
+          <h4>
+            Full Stack Web Developer based in Denver, Colorado. Having
+            experience in HTML, CCS, Javascript, Nodejs, Expressjs, and Reactjs.
+          </h4>
+        </div>
+        <KeyboardDoubleArrowDownIcon
+          fontSize="large"
+          color="disabled"
+          className="down-arrow"
+        />
+      </div>
     </div>
   );
 }
